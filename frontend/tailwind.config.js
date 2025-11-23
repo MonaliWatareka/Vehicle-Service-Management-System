a24226533d18b8491ff56eb1c82c1b4d6c8d4d68
Monali Watareka <monaliwatareka@gmail.com>
2025-11-23 14:05:36 +0530

    Add files via upload

---
+++ frontend/tailwind.config.js
@@ -0,0 +1,8 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./src/**/*.{js,jsx,ts,tsx}"], // Make sure this covers your React components
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+};
